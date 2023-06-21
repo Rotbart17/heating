ca9e436d5693b51a91e3b0fde81a08ab87dbdab3
Rotbart17 <dronte900@hotmail.com>
2023-06-21 19:25:34 +0200

    smal thinga

--- sensors.py
+++ sensors.py
@@ -15,7 +15,7 @@ class sensor:
 
     # connection zur DB
     conn = None
-    # Zeit bis zur nächsten Abfrage des Sensors in sec
+    # Zeit bis zur nächsten Abfrage eines Sensors in sec
     waittime = 300
 
     # Wert des Sensors
@@ -54,23 +54,23 @@ class sensor:
        
         try:
             self.conn = sqlite3.connect(settings.DBPATH)
-            logging.info('fDB-Verbindung geöffnet')
+            logging.info('DB-Verbindung geöffnet')
         except Error as e:
-            logging.error('fEs konnte keine Verbindung zu Datenbank erstellt werden. Programm wird beendet')
+            logging.error('Es konnte keine Verbindung zu Datenbank erstellt werden. Programm wird beendet')
             exit(1)
         try:
             c = self.conn.cursor()
             create_table_sql = sql_create_sensor_table_p1 + self.tn + sql_create_sensor_table_p2
             c.execute(create_table_sql)
-            logging.info('fTabelle' + self.tn +' erstellt')
+            logging.info('Tabelle' + self.tn +' erstellt')
         except Error as e:
-            logging.error('fEs konnte kein Cursor in der Datenbank erstellt werden um die Tabellen zu erzeugen. Programm wird beendet!')
+            logging.error('Es konnte kein Cursor in der Datenbank erstellt werden um die Tabellen zu erzeugen. Programm wird beendet!')
             exit(1)
         # so, die Tabelle existiert, jetzt noch die Sensorliste aufbauen
         # bsp: for f in SensorList:
         #          f.DoSomething()
         settings.SensorList.append(self)
-        logging.info('fSensor '+self.tn+' in die Sensorliste eingehängt!')
+        logging.info('Sensor '+self.tn+' in die Sensorliste eingehängt!')
         # ok jetzt ist eigetlich alles vorbereitet, jetzt noch die Sensorabfrage starten
         self.startthread
 
@@ -131,7 +131,7 @@ class sensor:
     def processvalue(self):
         while (not self.threadstop):
             self.rawtemp = self.getvalue()
-            self.temperature = self.convertvalue()
+            self.temperature = self.convertvalue(self.rawtemp)
             self.storevalue()
             time.sleep(self.waittime)
         # Wenn Ende dann abbrechen
@@ -152,23 +152,31 @@ class kesselsensor(sensor):
     tablename = "Kesselsensor"
     def __init__(self, tablename):
         super().__init__(tablename)
+        return
 
+
+    # Deconstructor wenn instanz gelöscht wird.
     def __del__(self):
         logging.debug('fSensor '+self.tn+'stoppen')
         return super().__del__()
         
 
     # hier muss die spezielle Abfrage für den Sensor bei der Vererbung eingesetzt werden
-    def getvalue(self):
-        pass
-        # Abfrage
-        # return (self.rawtemp)
-        logging.debug('fSensorwert '+self.tn+'abfragen')
+    def getvalue(self) -> float:
+        
+        if settings.V_Mode == True:
+            return(randomValue)
+        else:
+        #  Abfrage
+            logging.debug('Sensorwert '+self.tn+'abfragen')
+        return (self.rawtemp)
+        
 
 
     # Wert in Temperatur wandeln
-    def convertvalue(self):
-        pass
-        logging.debug('fSensorwert '+self.tn+'wandeln')
-
+    def convertvalue(self, self.rawtemp) -> float:
+        # wahllose umrechnung für den Kesselsensor habe ich keine Werte
+        temp = self.rawtemp *10
+        logging.debug('Sensorwert '+self.tn+'wandeln')
+        return (temp)
 
