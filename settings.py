d41f03baba6d4ac3daacb6041b62d37893fea966
Rotbart17 <dronte900@hotmail.com>
2023-06-29 15:47:03 +0200

    verringeren Code Sensorklassen

--- settings.py
+++ settings.py
@@ -31,6 +31,16 @@ SensorList = []
 # Liste aller Sensortabellen
 SENSORTABLELIST = ["Kesselsensor", "Aussensensor", "Innensensor", "Brauchwassersensor", "Brennersensor"]
 
+
+# Sensor Dictionary um jede Sensorklasse mit der richtigen Formel zu versorgen, sonst muss der 
+# Code für jede Class kopiert werden.
+sensordict = {
+    "Kesselsensor" : "(-7.79670769172508*pow(rt,3)) + (39.9983314997706*pow(rt,2)) + (-109.299890516815*rt) + 163.716704847826",
+    "Aussensensor" : "(-2.1331945*pow(rt,3)) + (16.44056044*pow(tr,2)) - (57.79703416*rt) + 104.0119689",
+    "Innensensor" : "",
+    "Brauchwassersensor" : "(-7.796707692*pow(rt,3)) + (39.9983315*pow(rt,2)) - (109.2998905*x) +163.7167048",
+}
+
 # Arbeitstabellenname
 tablename=None
 
